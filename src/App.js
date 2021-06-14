4cc76f604f2fa90248a38121a0fc1b3a1cdc31eb
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-14 09:11:26 +0100

    making things work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import bg from './images/test.jpg'
+import bgtwo from './images/oldmecca.jpg'
+import bgthree from './images/oldmeccatwo.jpg'
+import ReactPlayer from 'react-player/youtube'
+import { useState } from 'react';
+
 
 function App() {
+
+  const [isPlaying, setIsPlaying] = useState(true)
+
+  const [isMuted, setIsMuted] = useState(true)
+
+  const hiddenPlayer = {
+    pointerEvents: "none",
+    userSelect: "none",
+    position: "fixed",
+    top: "100%",
+    left: "100%",
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <div
+        id="vintage"
+        class="flex items-center justify-center min-h-screen "
+        style={{
+          backgroundImage: `url('${bgthree}')`,
+          backgroundSize: "cover",
+
+        }}>
+
+        <div>
+          <button
+            class="font-mono text-5xl	..."
+            onClick={() => setIsMuted(false)}> Click here to start</button>
+        </div>
+
+
+        <ReactPlayer
+          url='https://youtu.be/UaMPoBQr7uA?t=789'
+          style={hiddenPlayer}
+          playing={isPlaying}
+          controls="false"
+          // width="100vw"
+          // height="200vw"
+          muted={isMuted}
+          volume={1}
+          playsinline={true}
+
+        />
+
+
+
+      </div >
     </div>
+
   );
+
+
 }
 
 export default App;
---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navigation = () => {
+    return (
+        <div class="h-screen bg-purple-600 bg-opacity-100 ...">
+
+
+
+
+        </div>
+    )
+}
+
+export default navigation

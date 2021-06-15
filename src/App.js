0481b63d03669743b19c81ab8d3a43456022ce76
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-15 19:45:45 +0100

    added play and pause

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './index.css';
-import bg from './images/darker.jpg'
-import bgtwo from './images/oldmecca.jpg'
+import meccaone from './images/darker.jpg'
+import medina from './images/medina2.jpg'
 import bgthree from './images/oldmadinatwo.jpg'
 import mecca from './images/meccanew.jpg'
 import ReactPlayer from 'react-player/youtube'
 import { useEffect, useState } from 'react';
+import { motion } from "framer-motion"
+
 
 
 function App() {
@@ -31,7 +33,61 @@ function App() {
     <div
 
     >
+      <div className="flex py-16	 justify-center min-h-screen ">
+        <div class="text-center lg:w-2/3 w-full">
+          <button
+            class="text-3xl text-center font-black ...  font-extrabold	tracking-wide	 "
+          >  وَاِذَا قُرِئَ الۡقُرۡاٰنُ فَاسۡتَمِعُوۡا لَهٗ وَاَنۡصِتُوۡا لَعَلَّكُمۡ تُرۡحَمُوۡنَ‏  ۝ </button>
 
+
+          <p class="text-l py-3	text-center  font-mono  font-extrabold text-white-800 ">
+            So when the Qur'an is recited, listen carefully to it, and keep silent so that you may, be shown mercy.
+          </p>
+          <p class="text-l 	text-center  font-mono  font-extrabold text-white-800 ">
+            [7:204]
+          </p>
+          <div class="flex justify-center">
+
+            {isPlaying === false && (
+              <motion.button
+                onClick={() => setIsPlaying(true)}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }} >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="black">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </motion.button>
+            )}
+
+            {isPlaying === true && (
+              <motion.button
+                onClick={() => setIsPlaying(false)}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }} >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="black">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </motion.button>
+
+
+
+
+            )}
+
+
+            {/* <button
+              onClick={() => setPlay(false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+
+            </button> */}
+          </div>
+        </div>
+
+      </div>
 
       <div class="absolute top-0 left-0 right-9 flex items-center justify-between p-4">
         <div class="relative -top-0.5">
@@ -39,33 +95,19 @@ function App() {
             <span class="text-xs">
               Online
             </span>
-
-          </div>
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center min-h-screen ">
-        <div class="text-center lg:w-2/3 w-full">
-          <button
-            class="text-6xl text-center  font-mono text-white-500 "
-            onClick={() => setIsMuted(false)}> Click here to start </button>
-
-
-          <p class="leading-relaxed mb-8">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
-          <div class="flex justify-center">
-            <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-            <button class="ml-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
           </div>
         </div>
 
       </div>
+
+
 
 
 
       <div
         id='vignette'
         style={{
-          backgroundImage: `url('${darker}')`,
+          backgroundImage: `url('${meccaone}')`,
           backgroundSize: "cover",
           opacity: "0.7"
 
@@ -96,13 +138,12 @@ function App() {
             muted={isMuted}
             volume={1}
             playsinline={true}
-            onReady={() => { }}
+            onPlay={() => setIsMuted(false)}
             onPause={() => setIsPlaying(false)}
-
           />
         </div>
       </div>
-    </div>
+    </div >
 
 
   );

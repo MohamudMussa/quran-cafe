b25c59f028b50f961e57e482d68977431364005e
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-15 12:51:19 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './index.css';
-import bg from './images/vignette.png'
+import bg from './images/darker.jpg'
 import bgtwo from './images/oldmecca.jpg'
 import bgthree from './images/oldmadinatwo.jpg'
 import mecca from './images/meccanew.jpg'
@@ -28,58 +28,79 @@ function App() {
 
   return (
 
-    <div id="vintage">
+    <div
+
+    >
+
+
+      <div class="absolute top-0 left-0 right-9 flex items-center justify-between p-4">
+        <div class="relative -top-0.5">
+          <div class="flex items-center space-x-1">
+            <span class="text-xs">
+              Online
+            </span>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center min-h-screen ">
+        <div class="text-center lg:w-2/3 w-full">
+          <button
+            class="text-6xl text-center  font-mono text-white-500 "
+            onClick={() => setIsMuted(false)}> Click here to start </button>
+
+
+          <p class="leading-relaxed mb-8">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+          <div class="flex justify-center">
+            <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+            <button class="ml-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          </div>
+        </div>
+
+      </div>
 
 
 
       <div
-        id='darken'
-        className="flex items-center justify-center min-h-screen "
+        id='vignette'
         style={{
-          backgroundImage: `url('${bgthree}')`,
+          backgroundImage: `url('${darker}')`,
           backgroundSize: "cover",
-          opacity: "0.8"
+          opacity: "0.7"
 
         }}>
 
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded z-40" alt="hero" ></img>
-          <div class="text-center lg:w-2/3 w-full">
-            <button
-              class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"
-              onClick={() => setIsMuted(false)}> Click here to start</button>
+        <div
+          id='vin'>
+          <div
+            id='vignette'
+            class="flex flex-col items-center md:h-screen md:justify-center px-4 py-16 md:py-0">
 
-
-            <p class="leading-relaxed mb-8">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
-            <div class="flex justify-center">
-              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-              <button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
-            </div>
           </div>
+
+        </div >
+
+        <div>
+
+
+
+
+          <ReactPlayer
+            url='https://www.youtube.com/watch?v=bEGm_DHmiuk&list=PLxGJ20Im19wOWIfITK3KL69qm2j8kb3gI&ab_channel=%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1'
+            style={hiddenPlayer}
+            playing={isPlaying}
+            controls={false}
+            // width="100vw"
+            // height="200vw"
+            muted={isMuted}
+            volume={1}
+            playsinline={true}
+            onReady={() => { }}
+            onPause={() => setIsPlaying(false)}
+
+          />
         </div>
-      </div >
-
-      <div>
-
-
-
-
-        <ReactPlayer
-          url='https://www.youtube.com/watch?v=bEGm_DHmiuk&list=PLxGJ20Im19wOWIfITK3KL69qm2j8kb3gI&ab_channel=%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1'
-          style={hiddenPlayer}
-          playing={isPlaying}
-          controls={false}
-          // width="100vw"
-          // height="200vw"
-          muted={isMuted}
-          volume={1}
-          playsinline={true}
-          onReady={() => { }}
-          onPause={() => setIsPlaying(false)}
-
-        />
-
-
       </div>
     </div>
 

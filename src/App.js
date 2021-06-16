1e70a68c21eb01448c014dc69936db2827cead34
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-16 13:10:31 +0100

    yaaa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const IMAGES = [
 function App() {
 
   const getImage = () => {
-    const number = Math.floor(Math.random() * 2) + 1;
+    const number = Math.floor(Math.random() * 3);
     console.log(number);
     return IMAGES[number];
   };
@@ -56,7 +56,7 @@ function App() {
       >
         <div className="text-center lg:w-2/3 w-full">
           <h1
-            className="text-3xl text-center font-black ...  font-extrabold	tracking-wide	 "
+            className="text-3xl text-center font-black font-extrabold	tracking-wide	 "
 
           >
             وَاِذَا قُرِئَ الۡقُرۡاٰنُ فَاسۡتَمِعُوۡا لَهٗ وَاَنۡصِتُوۡا لَعَلَّكُمۡ تُرۡحَمُوۡنَ‏  ۝ </h1>
@@ -76,7 +76,7 @@ function App() {
               onClick={() => setVolume(volume + 0.2)}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }} >
-              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" /></svg>
             </motion.button>
 
             {isPlaying === false && (
@@ -85,7 +85,7 @@ function App() {
                 onClick={() => setIsPlaying(true)}
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }} >
-                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z" /></svg>
 
               </motion.button>
             )}
@@ -97,28 +97,18 @@ function App() {
                 onClick={() => setIsPlaying(false)}
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }} >
-                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1 17h-3v-10h3v10zm5-10h-3v10h3v-10z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1 17h-3v-10h3v10zm5-10h-3v10h3v-10z" /></svg>
 
               </motion.button>
-
-
-
-
             )}
 
 
             <motion.button
+              className="pr-4"
               onClick={() => setVolume(volume - 0.2)}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }} >
-              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" /></svg>
-            </motion.button>
-
-            <motion.button
-              onClick={handleClick}
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }} >
-              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"><path d="M5 8.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5zm9 .5l-2.519 4-2.481-1.96-4 5.96h14l-5-8zm8-4v14h-20v-14h20zm2-2h-24v18h24v-18z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" /></svg>
             </motion.button>
 
 
@@ -130,9 +120,15 @@ function App() {
       <div className="absolute top-0 left-0 right-9 flex items-center justify-between p-4">
         <div className="relative -top-0.5">
           <div className="flex items-center space-x-1">
-            <span className="text-xs">
-              Online
-            </span>
+
+            <motion.button
+
+              onClick={handleClick}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }} >
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24"><path d="M5 8.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5zm9 .5l-2.519 4-2.481-1.96-4 5.96h14l-5-8zm8-4v14h-20v-14h20zm2-2h-24v18h24v-18z" /></svg>
+            </motion.button>
+
           </div>
         </div>
 

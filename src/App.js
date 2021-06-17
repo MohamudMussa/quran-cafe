a2dcd68c02a89ecf403137dbe61a27da869aa23a
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-17 22:07:04 +0100

    you can now shuffle lad

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ const IMAGES = [
   meccaone, medina, meccatwo,
 ];
 
+const STATION = [
+  "hO5wCnVLHWU", "bEGm_DHmiuk&list=PLxGJ20Im19wOWIfITK3KL69qm2j8kb3gI&ab_channel=%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1", "C2BMIMWnt-c&ab_channel=UniqueQuranUniqueQuran",
+];
+
 function App() {
 
   const getImage = () => {
@@ -27,8 +31,27 @@ function App() {
     setActiveImage(image);
   };
 
+  // STATION
 
-  const [volume, setVolume] = useState(1)
+  const getStation = () => {
+    const numberStation = Math.floor(Math.random() * 3);
+    console.log(numberStation);
+    return STATION[numberStation];
+  };
+
+  const [station, setStation] = useState(STATION[0])
+
+
+  const handleNext = () => {
+    const station = getStation();
+    setStation(station);
+  };
+
+
+
+
+  const [volume, setVolume] = useState(0.5)
+
   const [isPlaying, setIsPlaying] = useState(false)
 
   const [isMuted, setIsMuted] = useState(true)
@@ -129,6 +152,14 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="32" viewBox="0 0 24 24"><path d="M5 8.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5zm9 .5l-2.519 4-2.481-1.96-4 5.96h14l-5-8zm8-4v14h-20v-14h20zm2-2h-24v18h24v-18z" /></svg>
             </motion.button>
 
+            <motion.button
+
+              onClick={handleNext}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }} >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 9v-3c-1 0-3.308-.188-4.506 2.216l-4.218 8.461c-1.015 2.036-3.094 3.323-5.37 3.323h-3.906v-2h3.906c1.517 0 2.903-.858 3.58-2.216l4.218-8.461c1.356-2.721 3.674-3.323 6.296-3.323v-3l6 4-6 4zm-9.463 1.324l1.117-2.242c-1.235-2.479-2.899-4.082-5.748-4.082h-3.906v2h3.906c2.872 0 3.644 2.343 4.631 4.324zm15.463 8.676l-6-4v3c-3.78 0-4.019-1.238-5.556-4.322l-1.118 2.241c1.021 2.049 2.1 4.081 6.674 4.081v3l6-4z" /></svg>
+            </motion.button>
+
           </div>
         </div>
 
@@ -164,7 +195,7 @@ function App() {
 
 
           <ReactPlayer
-            url='https://www.youtube.com/watch?v=bEGm_DHmiuk&list=PLxGJ20Im19wOWIfITK3KL69qm2j8kb3gI&ab_channel=%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1%D9%85%D8%AD%D8%A8%D8%A7%D9%84%D8%AE%D9%8A%D8%B1'
+            url={"https://www.youtube.com/watch?v=" + station}
             style={hiddenPlayer}
             playing={isPlaying}
             controls={false}

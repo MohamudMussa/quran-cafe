5514504230b88040313c15ddf8da8f7b0ceb12f0
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-18 10:24:00 +0100

    changes were made

--- src/App.js
+++ src/App.js
@@ -105,18 +105,18 @@ function App() {
     <div
 
     >
+
+
       <motion.div className="flex py-16	 justify-center min-h-screen "
         initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}
 
       >
-        <div class="absolute top-5 right-4 h-16 w-16 ...">
-          <TwitterShareButton
-            url={'https://quran.cafe/'}
-            options={{ text: `I'm currently Listen to Quran on @QuranCafe and you should too!`, via: 'qurancafe' }}
-          />
-        </div>
+
+
+
 
         <div className="text-center lg:w-2/3 w-full">
+
           <h1
             className="text-3xl text-center font-black font-extrabold	tracking-wide	 "
 
@@ -131,6 +131,7 @@ function App() {
             [7:204]
           </p>
           <div className="flex justify-center">
+
 
 
             <motion.button
@@ -173,6 +174,18 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" /></svg>
             </motion.button>
 
+            <div class="absolute top-5 right-4 h-16 w-16 ...">
+              {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /> */}
+              <TwitterShareButton
+                url={'https://quran.cafe/'}
+                options={{ text: `I'm currently Listen to Quran on @QuranCafe and you should too!`, via: 'qurancafe' }}
+              >
+              </TwitterShareButton>
+              {/* </svg> */}
+
+            </div>
+
+
 
           </div>
         </div>
@@ -198,8 +211,6 @@ function App() {
               whileTap={{ scale: 0.9 }} >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 9v-3c-1 0-3.308-.188-4.506 2.216l-4.218 8.461c-1.015 2.036-3.094 3.323-5.37 3.323h-3.906v-2h3.906c1.517 0 2.903-.858 3.58-2.216l4.218-8.461c1.356-2.721 3.674-3.323 6.296-3.323v-3l6 4-6 4zm-9.463 1.324l1.117-2.242c-1.235-2.479-2.899-4.082-5.748-4.082h-3.906v2h3.906c2.872 0 3.644 2.343 4.631 4.324zm15.463 8.676l-6-4v3c-3.78 0-4.019-1.238-5.556-4.322l-1.118 2.241c1.021 2.049 2.1 4.081 6.674 4.081v3l6-4z" /></svg>
             </motion.button>
-
-
 
           </div>
         </div>

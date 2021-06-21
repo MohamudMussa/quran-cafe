03399d051fa0901230908a0edb251c8d2e31567d
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-21 23:03:50 +0100

    update

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ function App() {
     >
 
 
-      <motion.div className="flex py-16	 justify-center md:h-screen min-h-screen  "
+      <motion.div className="flex py-16	 justify-center h-screen  "
         initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}
         style={{
           backgroundImage: `url('${activeImage}')`,

1d6e25a9428d746b1c923f97b47e663ea4b93680
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-06-22 11:48:17 +0100

    update

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { motion } from "framer-motion";
-// import { TwitterShareButton } from 'react-twitter-embed';
+import { TwitterShareButton } from 'react-twitter-embed';
 
 
 const variants = {
@@ -32,7 +32,7 @@ export const MenuItem = ({ i }) => {
             whileTap={{ scale: 0.95 }}
         >
 
-            {/* ABOUT ME ADDED
+            {/* ABOUT ME ADDED */}
             <div className="icon-placeholder" >
                 <h1 className="pb-3"> Quran Cafe ☕ </h1>
 
@@ -44,14 +44,14 @@ export const MenuItem = ({ i }) => {
                     style={{ color: 'gold' }} href="https://twitter.com/QuranCafe" rel="noreferrer">
                     DM @QuranCafe
                 </a>
-                <div className="pt-3"></div>
+                <div className="pb-10"></div>
             </div>
 
 
 
 
             <svg
-                className="pt-15"
+
                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
                 <path className="change-my-color" d="M18 9v-3c-1 0-3.308-.188-4.506 2.216l-4.218 8.461c-1.015 2.036-3.094 3.323-5.37 3.323h-3.906v-2h3.906c1.517 0 2.903-.858 3.58-2.216l4.218-8.461c1.356-2.721 3.674-3.323 6.296-3.323v-3l6 4-6 4zm-9.463 1.324l1.117-2.242c-1.235-2.479-2.899-4.082-5.748-4.082h-3.906v2h3.906c2.872 0 3.644 2.343 4.631 4.324zm15.463 8.676l-6-4v3c-3.78 0-4.019-1.238-5.556-4.322l-1.118 2.241c1.021 2.049 2.1 4.081 6.674 4.081v3l6-4z" /></svg>
             <p > Change Station </p>
@@ -74,7 +74,7 @@ export const MenuItem = ({ i }) => {
 
 
             </div>
- */}
+
 
 
 

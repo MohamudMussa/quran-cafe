10ac75c7b6a94e009548d8f1751bde0d39fb5cb5
Umar Gora <umarg1997@gmail.com>
2021-09-26 13:38:01 +0100

    added react-query and setup hydration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;

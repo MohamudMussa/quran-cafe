9e7f3b6832d26660e38da54214861701f98d108f
Mohamud Mussa <mmussa92@gmail.com>
2022-04-05 22:51:21 +0100

    action styles

--- components/player/actions.js
+++ components/player/actions.js
@@ -12,7 +12,7 @@ function Actions({
 }) {
   return (
     <div className="flex justify-center">
-      <motion.button
+      {/* <motion.button
         className="pr-4 z-40"
         onClick={incrementVolume}
         whileHover={{ scale: 1.1 }}
@@ -26,27 +26,27 @@ function Actions({
         >
           <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
         </svg>
-      </motion.button>
+      </motion.button> */}
 
       {playing ? (
         <motion.button
-          className="pr-4  z-40"
+          className="text-white p-8 rounded-full bg-black shadow-lg"
           onClick={handlePause}
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1 17h-3v-10h3v10zm5-10h-3v10h3v-10z" />
-          </svg>
+        <svg class="w-8 h-8" 
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 20 20">
+        <path d="M5 4h3v12H5V4zm7 0h3v12h-3V4z" />
+      
+        </svg>
+
         </motion.button>
       ) : (
         <motion.button
-          className="pr-4  z-40"
+          className="text-white p-8 rounded-full bg-black shadow-lg"
           onClick={handlePlay}
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
@@ -56,13 +56,14 @@ function Actions({
             width="30"
             height="30"
             viewBox="0 0 24 24"
+            fill="currentColor"
           >
             <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z" />
           </svg>
         </motion.button>
       )}
 
-      <motion.button
+      {/* <motion.button
         className="pr-4  z-40"
         onClick={decrementVolume}
         whileHover={{ scale: 1.1 }}
@@ -76,7 +77,7 @@ function Actions({
         >
           <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-12v-2h12v2z" />
         </svg>
-      </motion.button>
+      </motion.button> */}
       {/* <motion.button
         className="z-40 -mt-2"
         onClick={upvoteVideo}
